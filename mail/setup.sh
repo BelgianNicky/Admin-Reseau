60392f18f3ecd480449b9de1f8105bd45849e4e3
root <root@wt1-11.ephec-ti.be>
2020-06-01 17:18:34 +0200

    script mail

---
+++ mail/setup.sh
@@ -0,0 +1,8 @@
+curl -o Dockerfile https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mail/Dockerfile
+curl -o config.php https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mail/config.php
+curl -o hostname https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mail/hostname
+curl -o hosts https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mail/hosts
+curl -o init.sh https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mail/init.sh
+curl -o mailname https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mail/mailname
+curl -o main.cf https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mail/main.cf
+
