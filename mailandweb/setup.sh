39110d6dff1ed33e3513c5908bfb828dd1ccc626
root <root@wt1-11.ephec-ti.be>
2020-06-01 17:34:29 +0200

    ajout config mailandweb

---
+++ mailandweb/setup.sh
@@ -0,0 +1,8 @@
+curl -o Dockerfile https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mailandweb/Dockerfile
+curl -o config.php https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mailandweb/config.php
+curl -o hostname https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mailandweb/hostname
+curl -o hosts https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mailandweb/hosts
+curl -o init.sh https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mailandweb/init.sh
+curl -o mailname https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mailandweb/mailname
+curl -o main.cf https://raw.githubusercontent.com/BelgianNicky/Admin-Reseau/master/mailandweb/main.cf
+
